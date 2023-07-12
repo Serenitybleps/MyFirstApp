d6089552d50eced52e430acd1f6b64c800207d82
Serenitybleps <fawkesphoenix21@gmail.com>
2023-07-12 14:39:25 -0700

    Initial Commit

---
+++ MyFirstApp/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  MyFirstApp
+//
+//  Created by scholar on 7/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+       
+        VStack {
+            Text("Silly Goose")
+                .font(.title)
+                .foregroundColor(Color.black)
+            
+            Image("SillyGoose")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+              
+                
+        }
+        .padding()
+        
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
